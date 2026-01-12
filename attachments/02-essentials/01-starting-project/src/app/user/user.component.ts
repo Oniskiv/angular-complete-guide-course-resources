d488c9dd5fef4508be24cd3866e5078886f2ef54
Uladzislau Onyskiu <Uladzislau.Onyskiu@ihg.com>
2026-01-12 18:38:06 +0300

    Add user component

--- attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, computed, input} from '@angular/core';
 import {DUMMY_USERS} from "../dummy-users";
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,5 +11,13 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  imagePath = computed(() => 'assets/users/' + this.avatar());
+
+  onSelectUser() {
+  }
 }
