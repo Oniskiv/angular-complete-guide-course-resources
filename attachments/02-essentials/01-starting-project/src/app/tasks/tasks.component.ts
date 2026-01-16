fe44f5f9e32c4f369c0f2b7c14c0e7b351974ad8
Uladzislau Onyskiu <Uladzislau.Onyskiu@ihg.com>
2026-01-16 17:05:57 +0300

    Add task component

--- attachments/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
+++ attachments/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {TaskComponent} from "./task/task.component";
 import {NewTaskComponent} from "./new-task/new-task.component";
-import {NewTaskData} from "./task/task.model";
+import {TasksService} from "./tasks.service";
 
 @Component({
   selector: 'app-tasks',
@@ -18,22 +18,18 @@ export class TasksComponent {
   @Input({required: true}) name!: string;
   isAddingTask = false;
 
-  get selectedUserTasks() {
-    return this.tasks.filter(task => task.userId === this.userId);
+  constructor(private tasksService: TasksService) {
   }
 
-  onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter(task => task.id !== id);
+  get selectedUserTasks() {
+    return this.tasksService.getUserTasks(this.userId);
   }
 
   onStartAddTask() {
     this.isAddingTask = true;
   }
 
-  onCancelAddTask() {
+  onCloseAddTask() {
     this.isAddingTask = false;
-  }
-
-  onAddTask(taskData: NewTaskData) {
   }
 }
--- attachments/02-essentials/01-starting-project/src/app/tasks/tasks.service.ts
+++ attachments/02-essentials/01-starting-project/src/app/tasks/tasks.service.ts
@@ -1,6 +1,8 @@
 import {NewTaskData} from "./task/task.model";
+import {Injectable} from "@angular/core";
 
-class TasksService {
+@Injectable({providedIn: 'root'})
+export class TasksService {
   private tasks = [
     {
       id: 't1',
@@ -25,6 +27,14 @@ class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -37,9 +47,15 @@ class TasksService {
       summary: taskData.summary,
       dueDate: taskData.dueDate
     });
+    this.saveTasks();
   }
 
   removeTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
