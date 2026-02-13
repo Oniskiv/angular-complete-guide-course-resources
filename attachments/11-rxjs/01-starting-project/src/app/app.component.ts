75240fc51ba3bd01fc02c4eda60cf54028cacef5
Onyskiu, Uladzislau (EPAM) <Uladzislau.Onyskiu@ihg.com>
2026-02-13 17:23:36 +0300

    Module 11

---
+++ attachments/11-rxjs/01-starting-project/src/app/app.component.ts
@@ -0,0 +1,66 @@
+import {Component, DestroyRef, inject, OnInit, signal} from '@angular/core';
+import {toObservable, toSignal} from "@angular/core/rxjs-interop";
+import {interval, Observable} from "rxjs";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit {
+  clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount);
+  interval$ = interval(1000);
+  intervalSignal = toSignal(this.interval$, {initialValue: 0});
+  customInterval$ = new Observable((subscriber) => {
+    let timesExecuted = 0;
+    const interval = setInterval(() => {
+      if (timesExecuted > 3) {
+        clearInterval(interval);
+        subscriber.complete();
+        return;
+      }
+      console.log("Emitting new value...");
+      subscriber.next({message: 'New value'});
+      timesExecuted++;
+    }, 2000)
+  });
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    // effect(() => {
+    //   console.log(`Clicked button ${this.clickCount()} times`);
+    // });
+  }
+
+  ngOnInit(): void {
+    /*const subscription = interval(1000)
+      .pipe(
+        map((val) => val * 2)
+      )
+      .subscribe({
+        next: (val) => console.log(val)
+      });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })*/
+    this.customInterval$.subscribe({
+      next: value => {
+        console.log(value);
+      },
+      complete: () => console.log("Completed")
+    })
+    const subscription = this.clickCount$.subscribe({
+      next: value => console.log(`Clicked button ${value} times`)
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
+
+  onClick() {
+    this.clickCount.update(prevCount => prevCount + 1);
+  }
+}
